995abf7b7ef1a769b8e553f5667b99df80d7d78d
Kévin Rocher / @Darklg <Darklg@users.noreply.github.com>
2018-03-14 18:42:16 +0100

    Create wpupopin.php

---
+++ wpupopin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: WPU Popin
+Description: Add a popin on your user's first visit
+Version: 0.1.0
+Author: Darklg
+Author URI: http://darklg.me/
+License: MIT License
+License URI: http://opensource.org/licenses/MIT
+*/
